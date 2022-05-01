4fbdd52a5f97ddb72dd6d6d713ae7a0fa344b318
shemanisha <96568158+shemanisha@users.noreply.github.com>
2022-05-01 19:28:23 +0530

    added routes and env folder in backend folder

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
-app.get("/", (req, res) => {
-  res.send("hello from home");
+app.use(cors());
+
+readdirSync("./routes").map((route) => {
+  app.use("/", require("./routes/" + route));
 });
-app.listen(8000, () => {
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
   console.log("App is listening on port 8000");
 });
