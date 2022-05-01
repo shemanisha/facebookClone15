908b8b25617ff16d38b0d9bbeb1102e7caff16f6
shemanisha <shetemanisha15@gmail.com>
2022-05-01 21:29:10 +0530

    added mongoose connection code in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const { readdirSync } = require("fs");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 app.use(cors());
 
+//routes
 readdirSync("./routes").map((route) => {
   app.use("/", require("./routes/" + route));
 });
+
+//database
+mongoose
+  .connect(process.env.DATABASE_URL, {})
+  .then(() => {
+    console.log("Connection is successful");
+  })
+  .catch((err) => console.log("error connecting to database", err.message));
 
 const PORT = process.env.PORT || 8000;
 
