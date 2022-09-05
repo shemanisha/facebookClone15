aa67572878af383bed36ce0dff9747dd5c284bb3
shemanisha <96568158+shemanisha@users.noreply.github.com>
2022-09-05 11:48:30 +0530

    [frontend- Installed react-router-dom , added routerLinks in app.js , installed react-redux , created store in app .js ,created combined reducer and user reducer]

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+//css imports
 import "./index.css";
 import "./styles/icons/icons.css";
+//js imports
+
 import App from "./App";
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+const store = createStore(rootReducer, composeWithDevTools());
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
